c6b947d4d335f310b1e7617aff4cf2c72076b96f
Gregory Favre <Gregory.Favre@gmail.com>
2014-02-10 17:32:02 +0100

    added humans.txt, google identification and friends

--- beyondthewall/beyondthewall/urls.py
+++ beyondthewall/beyondthewall/urls.py
@@ -1,13 +1,30 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
+from django.conf import settings
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+class TextPlainView(TemplateView):
+    def render_to_response(self, context, **kwargs):
+        mimetype = 'text/plain; charset=utf-8'
+        return super(TextPlainView, self).render_to_response(context, 
+                                                             content_type=mimetype,
+                                                             **kwargs)
+
+
+
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^contact/', include('contact.urls')),
+    
+    url(r'^robots\.txt$', TextPlainView.as_view(template_name='robots.txt')),
+    url(r'^humans\.txt$', TextPlainView.as_view(template_name='humans.txt')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.ico')),
+    (r'^google7f92da28d3dd66e7\.html$', lambda r: HttpResponse("google-site-verification: google7f92da28d3dd66e7.html", mimetype="text/plain")),
+
     # Examples:
     # url(r'^$', 'beyondthewall_project.views.home', name='home'),
     # url(r'^beyondthewall_project/', include('beyondthewall_project.foo.urls')),
